eef86318c0bc7f343f822e7688a77e504342dc68
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-01-02 00:35:30 +0200

    added scaffolding for offer

---
+++ Exchanger/Controllers/OfferController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Exchanger.Data;
+using Exchanger.Models;
+
+namespace Exchanger.Controllers
+{
+    public class OfferController : Controller
+    {
+        private readonly ExchangerContext _context;
+
+        public OfferController(ExchangerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Offer
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Offer.ToListAsync());
+        }
+
+        // GET: Offer/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Offer == null)
+            {
+                return NotFound();
+            }
+
+            var offer = await _context.Offer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+
+            return View(offer);
+        }
+
+        // GET: Offer/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Offer/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdProfile,Title,Description,Images")] Offer offer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(offer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(offer);
+        }
+
+        // GET: Offer/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Offer == null)
+            {
+                return NotFound();
+            }
+
+            var offer = await _context.Offer.FindAsync(id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+            return View(offer);
+        }
+
+        // POST: Offer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdProfile,Title,Description,Images")] Offer offer)
+        {
+            if (id != offer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(offer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OfferExists(offer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(offer);
+        }
+
+        // GET: Offer/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Offer == null)
+            {
+                return NotFound();
+            }
+
+            var offer = await _context.Offer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+
+            return View(offer);
+        }
+
+        // POST: Offer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Offer == null)
+            {
+                return Problem("Entity set 'ExchangerContext.Offer'  is null.");
+            }
+            var offer = await _context.Offer.FindAsync(id);
+            if (offer != null)
+            {
+                _context.Offer.Remove(offer);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OfferExists(int id)
+        {
+          return _context.Offer.Any(e => e.Id == id);
+        }
+    }
+}
