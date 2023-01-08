28da4ffd7b77bc282fb0cc8d857bc0a1becf5b6e
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-01-08 05:33:02 +0200

    added pagining on global offers

---
+++ Exchanger/Models/ListPaginated.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Exchanger.Models
+{
+    public class ListPaginated<T> : List<T>
+    {
+        public int Index { get; private set; }
+        public int Total { get; private set; }
+
+        public ListPaginated(List<T> items, int count, int index, int size)
+        {
+            Index = index;
+            Total = (int)Math.Ceiling(count / (double)size);
+
+            AddRange(items);
+        }
+
+        public bool HasPreviousPage => Index > 1;
+
+        public bool HasNextPage => Index < Total;
+
+        public static async Task<ListPaginated<T>> CreateAsync(IQueryable<T> source, int index, int size = 50)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((index - 1) * size).Take(size).ToListAsync();
+            return new ListPaginated<T>(items, count, index, size);
+        }
+    }
+}
